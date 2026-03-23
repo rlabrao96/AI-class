6d654409d2c11a0b0dc4caae4f4d558bc7f891f4
rlabrao96 <rafaellabra96@gmail.com>
2026-03-22 21:37:57 -0400

    feat: register Quiz, AnimatedDiagram, ResourceCard in MDX global components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,13 @@
 import type { MDXComponents } from 'mdx/types'
+import { Quiz } from '@/components/Quiz'
+import { AnimatedDiagram } from '@/components/AnimatedDiagram'
+import { ResourceCard } from '@/components/ResourceCard'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return { ...components }
+  return {
+    ...components,
+    Quiz,
+    AnimatedDiagram,
+    ResourceCard,
+  }
 }
